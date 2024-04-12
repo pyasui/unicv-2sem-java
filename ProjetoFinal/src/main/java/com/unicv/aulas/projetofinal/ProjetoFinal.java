e22b4414631f803a98a1805da103c0cb8df48baf
pedro yasui <pedro.yasui@gmail.com>
2024-04-11 21:02:24 -0300

    first commit

---
+++ ProjetoFinal/src/main/java/com/unicv/aulas/projetofinal/ProjetoFinal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unicv.aulas.projetofinal;
+
+/**
+ *
+ * @author pedro
+ */
+public class ProjetoFinal {
+
+    public static void main(String[] args) {
+        HomePage form = new HomePage();
+        form.setVisible(true);
+    }
+}
