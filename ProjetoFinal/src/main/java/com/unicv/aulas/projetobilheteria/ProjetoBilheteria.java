1d33c4d9a0b0eb15ffd6e684a38b5df710d772b7
pedro yasui <pedro.yasui@gmail.com>
2024-05-23 21:25:35 -0300

    criar projeto

---
+++ ProjetoFinal/src/main/java/com/unicv/aulas/projetobilheteria/ProjetoBilheteria.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unicv.aulas.projetobilheteria;
+
+/**
+ *
+ * @author pedro
+ */
+public class ProjetoBilheteria {
+
+    public static void main(String[] args) {
+        var home = new FormHome();
+        home.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+        home.setVisible(true);
+    }
+}
---
+++ ProjetoFinal/src/main/java/com/unicv/aulas/projetobilheteria/classes/Estado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unicv.aulas.projetobilheteria.classes;
+
+/**
+ *
+ * @author pedro
+ */
+public class Estado {
+    public int id;
+    public String sigla;
+    public String nome;
+}
